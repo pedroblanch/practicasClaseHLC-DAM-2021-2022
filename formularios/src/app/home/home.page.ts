2b92255bda4f9fccae95216edf876eaadc3c46b0
pedroblanch <pedroblanch@iesjulioverne.es>
2021-11-03 09:21:03 +0100

    Miercoles 3 Noviembre

---
+++ formularios/src/app/home/home.page.ts
@@ -0,0 +1,55 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { NavigationExtras } from '@angular/router';
+import { NavController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  constructor(public formBuilder: FormBuilder,
+    private navCtrl: NavController) { }
+
+  validations_form: FormGroup;
+  genders: Array<string>;
+
+  ngOnInit() {
+
+    this.genders = [
+      "Male",
+      "Female"
+    ];
+
+    this.validations_form = this.formBuilder.group({
+      username: new FormControl('', Validators.compose([
+        Validators.maxLength(25),
+        Validators.minLength(5),
+        Validators.pattern('^[a-zA-Z]{1}[a-zA-Z0-9]+$'),
+        Validators.required
+      ])),
+      name: new FormControl('', Validators.required),
+      lastname: new FormControl('', Validators.required),
+      email: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.pattern('^[a-zA-Z0-9_.+-]+[@]{1}[a-zA-Z0-9-]+[.]{1}[a-zA-Z]+$')
+      ])),
+      gender: new FormControl(this.genders[0], Validators.required),
+      terms: new FormControl(false, Validators.pattern('true'))
+    });
+  }//end_ngOnInit
+
+  onSubmit(values) {
+    console.log(values.username);
+    let navigationExtras: NavigationExtras = {
+      queryParams: {
+        user: JSON.stringify(values),
+        numero: 3
+      }
+    };
+    this.navCtrl.navigateForward('/user', navigationExtras);
+  }
+
+}//end_Class
